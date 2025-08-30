56e3785554cfdfac43a4cc0de2dc67bcdb379a62
MKieburtz <michaelkieburtz@gmail.com>
2025-08-30 15:18:51 -0700

    make mudsurge work with any earthbendable block

--- src/com/jedk1/jedcore/ability/earthbending/MudSurge.java
+++ src/com/jedk1/jedcore/ability/earthbending/MudSurge.java
@@ -288,7 +288,7 @@ public class MudSurge extends EarthAbility implements AddonAbility {
 		for (Location l : area) {
 			Block b = l.getBlock();
 
-			if (isMudBlock(b)) {
+			if (isEarthbendable(b)) {
 				if (isTransparent(b.getRelative(BlockFace.UP))) {
 					boolean water = true;
 
